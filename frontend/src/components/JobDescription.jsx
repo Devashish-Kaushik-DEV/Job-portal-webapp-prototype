2586b0ef6a9aae97679b2c650832dca4bd047eab
Devashish-Kaushik-DEV <devashishuk@outlook.com>
2025-08-12 00:06:22 +0530

    working on UpdateProfileDialog but it has a few bugs right now

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
 
 const JobDescription = () => {
+  const isApplied = true;
   return (
-    <div>JobDescription</div>
-  )
-}
+    <div className="max-w-7xl mx-auto my-10">
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="font-bold text-xl">Frontend Developer</h1>
+          <div className="flex items-center gap-2 mt-4">
+            <Badge className="text-blue-700 font-bold" variant="ghost">
+              Positions
+            </Badge>
+            <Badge className="text-red-600 font-bold" variant="ghost">
+              Job Type
+            </Badge>
+            <Badge className="text-violet-900 font-bold" variant="ghost">
+              --LPA
+            </Badge>
+          </div>
+        </div>
+        <Button
+          onClick={(e) => {
+            if (isApplied) e.preventDefault(); // Prevent click action
+          }}
+          className={`rounded-lg ${
+            isApplied
+              ? "bg-gray-600 cursor-not-allowed"
+              : "bg-violet-800 hover:bg-[#431386]"
+          }`}
+        >
+          {isApplied ? "Already Applied" : "Apply Now"}
+        </Button>
+      </div>
+      <h1 className="border-b-2 border-b-gray-300 font-medium my-4 py-2">Job Description</h1>
+      <div>
+          <h1 className="font-bold my-1">Role: <span className="pl-4 font-normal text-gray-800">Frontend Developer</span></h1>
+          <h1 className="font-bold my-1">Location: <span className="pl-4 font-normal text-gray-800">Delhi</span></h1>
+          <h1 className="font-bold my-1">Description: <span className="pl-4 font-normal text-gray-800">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores perferendis, nam nemo ratione atque quidem?</span></h1>
+          <h1 className="font-bold my-1">Experience: <span className="pl-4 font-normal text-gray-800">2 years</span></h1>
+          <h1 className="font-bold my-1">Salary: <span className="pl-4 font-normal text-gray-800">9.6LPA</span></h1>
+          <h1 className="font-bold my-1">Total Applicants: <span className="pl-4 font-normal text-gray-800">8</span></h1>
+          <h1 className="font-bold my-1">Posted Date: <span className="pl-4 font-normal text-gray-800">10-08-2025</span></h1>
+      </div>
+    </div>
+  );
+};
 
-export default JobDescription
+export default JobDescription;
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./shared/Navbar";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
@@ -6,10 +6,13 @@ import { Contact, Mail, Pen } from "lucide-react";
 import { Badge } from "./ui/badge";
 import { Label } from "./ui/label";
 import AppliedJobTable from "./AppliedJobTable";
+import UpdateProfileDialog from "./UpdateProfileDialog";
 
 const skills = ["Html", "CSS", "JavaScript", "Python"];
+const isResume = true;
+
 const Profile = () => {
-  const isResume = true;
+  const [open, setOpen] = useState(false);
   return (
     <div>
       <Navbar />
@@ -27,7 +30,7 @@ const Profile = () => {
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
             </div>
           </div>
-          <Button className="text-right" variant="outline">
+          <Button onClick={() => setOpen(true)} className="text-right" variant="outline">
             <Pen />
           </Button>
         </div>
@@ -70,6 +73,7 @@ const Profile = () => {
         <h1 className="font-bold text-lg my-5">Applied Jobs</h1>
         <AppliedJobTable />
       </div>
+      <UpdateProfileDialog open={open} setOpen={setOpen} />
     </div>
   );
 };
